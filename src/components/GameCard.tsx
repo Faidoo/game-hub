8fecf2f7812450458a162dc93fb5e443753ccffa
Faisal Nizamudeen <m.f.nizamudeen@student.utwente.nl>
2023-12-30 01:22:38 +0100

    Added bottom margins and Cropped Images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageURL from "../services/img-url";
 
 interface Props {
   game: Game;
@@ -11,10 +12,10 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius="1rem" overflow="hidden">
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageURL(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack marginBottom={"3rem"} justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIconList>
